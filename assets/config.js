c0b24bd4a3a4dabaa05b4bed66d1742579294f5d
codebybiya <codebybiya@gmail.com>
2024-10-01 11:14:37 -0700

    ip

--- assets/config.js
+++ assets/config.js
@@ -5,13 +5,13 @@ const config = {
   baseUrl:
     Platform.OS === "web"
       ? "http://localhost:8081/aviaxin"
-      : "http://192.168.1.101:8081/aviaxin",
+      : "http://192.168.0.47:8081/aviaxin",
   backendUrl:
     Platform.OS === "web"
-      ? "http://192.168.1.104:8080/aviaxin"
-      : "http://192.168.1.104:8080/aviaxin",
-  //  ? "https://aviaxin-api-75cc816c85ed.herokuapp.com/aviaxin"
-  // : "https://aviaxin-api-75cc816c85ed.herokuapp.com/aviaxin",
+      ? // ? "http://192.168.1.104:8080/aviaxin"
+        // : "http://192.168.1.104:8080/aviaxin",
+        "https://aviaxin-api-75cc816c85ed.herokuapp.com/aviaxin"
+      : "https://aviaxin-api-75cc816c85ed.herokuapp.com/aviaxin",
 };
 
 export default config;
